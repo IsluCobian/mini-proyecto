36689945778eeb9526799920676c133f1320907e
Luis Cobian <luigui.angel07@hotmail.com>
2025-06-02 17:37:36 -0600

    chore: modify vite.config.js to include unplugin-auto-import and unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import RekaResolver from "reka-ui/resolver"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      dirs: ["./src/components"],
+    }),
+    AutoImport({
+      dirs: ["./src/composables"],
+      imports: ["vue"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

cf89fd3c948aa8ffe240ff51682770cefc36371f
Luis Cobian <luigui.angel07@hotmail.com>
2025-06-03 16:30:22 -0600

    refactor: move products declaration inside useProducts function and update addProduct method

--- src/composables/useProducts.js
+++ src/composables/useProducts.js
@@ -1,10 +1,15 @@
 import data from "@/data/products.json"
 import { ref } from "vue"
+const products = ref([...data.products])
 
 export function useProducts() {
-  const products = ref([...data.products])
+  const addProduct = (product) => {
+    product.id = Date.now()
+    products.value.push(product)
+  }
 
   return {
     products,
+    addProduct,
   }
 }

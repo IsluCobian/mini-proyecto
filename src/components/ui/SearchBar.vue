3623028ae912d915a0e638f1e0b8a2d31e9adcbb
Luis Cobian <luigui.angel07@hotmail.com>
2025-06-05 13:33:02 -0600

    feat(SearchBar): add search icon

--- src/components/ui/SearchBar.vue
+++ src/components/ui/SearchBar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, watch } from "vue"
+import { Search } from "lucide-vue-next"
 
 const modelValue = defineModel() // Enables v-model usage
 
@@ -15,10 +16,15 @@ watch(input, (newVal) => {
 </script>
 
 <template>
-  <input
-    v-model="input"
-    type="text"
-    placeholder="Buscar producto..."
-    class="focus:ring-primary/40 rounded-md border px-3 py-2 text-sm focus:ring-2 focus:outline-none"
-  />
+  <div class="relative flex">
+    <Search
+      class="text-muted-foreground absolute top-1/2 left-3 size-3.5 -translate-y-1/2"
+    />
+    <input
+      v-model="input"
+      type="text"
+      placeholder="Buscar producto..."
+      class="focus:ring-primary/40 rounded-md border px-3 py-2 pl-9.5 text-sm focus:ring-2 focus:outline-none"
+    />
+  </div>
 </template>

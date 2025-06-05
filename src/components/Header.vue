ceab4bcfc1dc99959bd0ace6b3bcf6f85294326a
Luis Cobian <luigui.angel07@hotmail.com>
2025-06-05 13:33:28 -0600

    fix(Header): remove unnecessary border from header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle.vue"
 
 <template>
   <header
-    class="bg-primary flex h-14 w-full items-center justify-between border-r p-4 text-white md:mr-1 md:h-full md:w-14 md:flex-col"
+    class="bg-primary flex h-14 w-full items-center justify-between p-4 text-white md:mr-1 md:h-full md:w-14 md:flex-col"
   >
     <h1 class="text-xl leading-none font-bold">SF</h1>
     <ThemeToggle />
